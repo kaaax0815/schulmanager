382991eb233d27a10de227c1b6b8e20a3a75e4c9
kaaax0815 <999999bst@gmail.com>
2022-12-23 16:56:48 +0100

    spelling, linting

--- packages/website/pages/login.tsx
+++ packages/website/pages/login.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
+
 export default function Login() {
   return (
     <main>
       <h1>kaaaxcreators&apos; Schulmanager</h1>
       <h2>Alternativer Client für Schulmanager Online</h2>
-      <a href="/api/auth/login">Einloggen</a>
+      <Link href="/api/auth/login">Einloggen</Link>
     </main>
   );
 }
